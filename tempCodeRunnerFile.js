6c260ca5a2ea2d60e2bcf9d0d163e60d855bb81c
Jayavel <jayavel1520@gmail.com>
2025-09-21 13:31:55 +0530

    Message Board Backend server

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,2 @@
-req.on('end', () => {
-        //     const bufferText = Buffer.concat(body).toString();
-        //     const message = bufferText.replace('+', " ");
-        //     console.log(message.split("=")[1]);
-
-        //     fs.writeFileSync('./data.txt', message, 'utf-8');
-
-        // })
+res.statusCode = 302;
+        res.setHeader("Location", "/");
